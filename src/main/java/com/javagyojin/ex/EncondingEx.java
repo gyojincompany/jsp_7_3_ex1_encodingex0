5e0ac981f70c5d687bf2987875d4ba846781cb5d
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-09-02 17:31:29 +0900

    한글인코딩 v1.0

---
+++ src/main/java/com/javagyojin/ex/EncondingEx.java
@@ -0,0 +1,73 @@
+package com.javagyojin.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EncondingEx
+ */
+@WebServlet("/EncondingEx")
+public class EncondingEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EncondingEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("GET 방식으로 실행되었습니다!");
+		
+		String name = request.getParameter("name");
+		
+		response.setContentType("text/html; charset=EUC-Kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html><head></head><body>");
+		writer.println("안녕하세요." + name + "님 반갑습니다.<br/>");
+		writer.println("</body></html>");
+		
+		writer.close();
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// doGet(request, response);
+		System.out.println("POST 방식으로 실행되었습니다!");
+		
+		request.setCharacterEncoding("EUC-KR");//request 객체의 파라미터값을 한글로 인코딩
+		
+		String name = request.getParameter("name");
+		
+		response.setContentType("text/html; charset=EUC-Kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html><head></head><body>");
+		writer.println("안녕하세요." + name + "님 반갑습니다.<br/>");
+		writer.println("</body></html>");
+		
+		writer.close();
+		
+	}
+
+}
